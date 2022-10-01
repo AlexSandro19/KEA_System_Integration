59d6f802006b1ac4f45b68e6702c6980834b7c63
Alex <a.sandrovschii@gmail.com>
2022-10-01 23:15:15 +0200

    fixed typos

--- Lecture_3/REST_API_Docs/fastapi_swagger/main.py
+++ Lecture_3/REST_API_Docs/fastapi_swagger/main.py
@@ -11,7 +11,7 @@ def _(item_id: int):
     return {"item_id": item_id}
 
 @app.get("/items") 
-def _(page: int = Query(default=1, gt=0)):  # quary parameter
+def _(page: int = Query(default=1, gt=0)):  # query parameter
     return {"page": page}
 
 # @app.get("/items") 
@@ -19,5 +19,5 @@ def _(page: int = Query(default=1, gt=0)):  # quary parameter
 #     return {"page": page}
 
 @app.get("/student/{email}") # path variable
-def _(email: str):  # quary parameter
+def _(email: str):  # query parameter
     return {"student": email}
